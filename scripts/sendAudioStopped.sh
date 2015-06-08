9dda03f1ef1f57c3b9fe287447eb6e40b60e9912
waynemerricks <waynemerricks@thevoiceasia.com>
2015-06-08 11:46:12 +0100

    Create sendAudioStopped.sh
    Script to log audio stopped event, light error led and send an email

---
+++ scripts/sendAudioStopped.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Script to amend GPIO and then send an email via sendemail with TLS enabled
+FROM="picodec001@your_mail_host"
+TO="your_alert_address"
+SUBJECT="PICODEC001 Audio Stopped!"
+MESSAGE="Hi\n\nI've lost audio.\n\nBoo!"
+USER="pi_mail_user"
+PASS="pi_mail_password"
+SERVER="your_mail_server"
+PORT=25
+
+#Add event to syslog
+logger "PiCodec - Playback Stopped"
+
+#Turn off ready LED
+/usr/local/bin/gpio -g write 4 0
+
+#Turn on error LED
+/usr/local/bin/gpio -g write 17 1
+
+#sendemail below should be all on one line
+sendemail -f "$FROM" -t "$TO" -u "$SUBJECT" -m "$MESSAGE" -s "$SERVER":"$PORT" -o tls= no -xu "$USER" -xp "$PASS"
